fba6307a9c868ea4e32792cc239abcacaa621810
Kristijan <92918561+kristijan00b@users.noreply.github.com>
2023-11-19 09:28:14 +0100

    Update app.component.ts
    Automatski generisan kod - visak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,8 +24,5 @@ interface Relacija {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
-  }
   relacija: Relacija = jsonData;
 }
